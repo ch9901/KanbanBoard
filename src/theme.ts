00cdb6667ff48690d179ead376eaa8363d8750f1
ch9901 <jcy990113@gmail.com>
2024-01-07 21:48:36 +0900

    last commit

---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "linear-gradient(90deg, #DE6262 0%, #ffb88c 100%);",
+  boardColor: "rgba(255,255,255,0.5)",
+  cardColor: "#fff",
+};
